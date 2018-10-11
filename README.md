3d23297205d1ec073b0f7deafcdfd64f32b1e497
Maxime Daniel <root@maxux.net>
2018-10-11 18:59:37 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,2 @@
+# Where Is My IP
+Small webservice using GeoLite2 database to give geographic information about IP address location
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+	"net"
+)
+
+type ipresponse struct {
+	CountryCode string  `json:"country_code"`
+	CountryName string  `json:"country_name"`
+	Subdivision string  `json:"subdivision"`
+	Continent   string  `json:"continent"`
+	CityName    string  `json:"city_name"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+	Source      string  `json:"source"`
+}
+
+func main() {
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		ip, _, err := net.SplitHostPort(c.Request.RemoteAddr)
+		if err != nil {
+			log.Printf("debug: Getting req.RemoteAddr %v", err)
+			c.JSON(500, gin.H{"error": "could not determine ip address"})
+		}
+
+		ipobj := net.ParseIP(ip)
+		record, err := db.City(ipobj)
+
+		if err != nil {
+			log.Printf("%v", err)
+			c.JSON(500, gin.H{"error": "db query failed"})
+		}
+
+		response := ipresponse{
+			record.Country.IsoCode,
+			"Unknown",
+			"Unknown",
+			"Unknown",
+			"Unknown",
+			record.Location.Latitude,
+			record.Location.Longitude,
+			"maxmind geolite2", // for credits
+		}
+
+		if val, ok := record.Country.Names["en"]; ok {
+			response.CountryName = val
+		}
+
+		if val, ok := record.Continent.Names["en"]; ok {
+			response.Continent = val
+		}
+
+		if val, ok := record.City.Names["en"]; ok {
+			response.CityName = val
+		}
+
+		if len(record.Subdivisions) > 0 {
+			response.Subdivision = record.Subdivisions[0].Names["en"]
+		}
+
+		c.JSON(200, response)
+	})
+
+	log.Print("Starting web server")
+	r.Run(":8085")
+}
