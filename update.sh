5bdd52ea11259699e3474939af003bf46f961237
Maxime Daniel <root@maxux.net>
2022-08-18 20:08:04 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,13 @@
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+#!/bin/bash
+set -ex
+
+LICENCE_KEY=xxxx
+
+curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${LICENCE_KEY}&suffix=tar.gz" -o GeoLite2-City-latest.tar.gz
+
+tar -xzvf GeoLite2-City-latest.tar.gz
+version=$(tar -tf GeoLite2-City-latest.tar.gz | head -1)
+
+cp -v $version/GeoLite2-City.mmdb GeoLite2-City.mmdb
+
+docker restart hub-geoip
